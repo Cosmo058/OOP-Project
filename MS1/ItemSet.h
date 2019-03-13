b1fa9e6f299fbfed114e0edf125dd5fb9514f597
Angel Sanchez Valencia <angelsanchez058@gmail.com>
2019-03-13 06:03:45 -0400

    MS1

---
+++ MS1/ItemSet.h
@@ -0,0 +1,30 @@
+// Workshop 6
+// ItemSet.h
+// Student Name : Angel Sanchez
+// Student #: 153582176
+// Student Id: asanchez-valencia
+// 12/Mar/2019
+
+#ifndef SICT_ITEMSET_H
+#define SICT_ITEMSET_H
+
+#include "Utilities.h"
+
+namespace sict {
+	class ItemSet {
+		std::string name;
+		unsigned int serial_num;
+		unsigned int quantity;
+		std::string desc;
+
+	public:
+		ItemSet(const std::string);
+		const std::string& getName() const;
+		const unsigned int getSerialNumber() const;
+		const unsigned int getQuantity() const;
+		ItemSet& operator--();
+		void display(std::ostream&, bool) const;
+	};
+}
+
+#endif // !SICT_ITEMSET_H
